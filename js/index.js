0537a5f78316403ed0ba2b791e562999461feb49
Budin <apserbud22@gmail.com>
2022-07-23 15:11:04 +0300

    done class

--- js/index.js
+++ js/index.js
@@ -1,33 +1,7 @@
-'use strict'
+'use strict';
+import { Controller } from "./Class/Controller.js";
+import { Model } from "./Class/Model.js";
+import { View } from "./Class/View.js";
 
-let user = {
-    data: {
-        a: 1,
-        b: 2,
-        c: 3,
-        d: {
-            a1: 1,
-            b1: 2,
-            c1: 3,
-            d1: {
-                a2: 3,
-                b2: 3,
-                c2: 3,
-            }
-        },
-    }
-}
-
-const deepFreeze = obj => {
-    Object.freeze(obj)
-    for( let key in obj) {
-        if(obj[key] instanceof Object) {
-            return deepFreeze(obj[key])
-        }
-    }
-}
-deepFreeze(user)
-
-console.log(Object.getOwnPropertyDescriptor(user, 'data'));
-console.log(Object.getOwnPropertyDescriptor(user.data, 'd'));
-console.log(Object.getOwnPropertyDescriptor(user.data.d, 'd1'));
+const app = new Controller(new Model(), new View());
+app.start()
